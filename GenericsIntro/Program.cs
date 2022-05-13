8e8110670d96ce02942a5909c0c0cac7f672a094
burak <burak@DESKTOP-RFGE9RF>
2022-05-13 08:37:22 +0300

    Add project files.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MYList<string> isimler = new MYList<string>();
+            isimler.Add("Engin");
+            
+        }
+    }
+}
